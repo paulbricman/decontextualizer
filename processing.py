eaa3c5e62a7dcf17a18b5e5e47f5a0db4b221567
Paul Bricman <paulbricman@protonmail.com>
2021-12-06 11:34:47 +0100

    fix: nasty local run of decontextualization T5

--- processing.py
+++ processing.py
@@ -1,6 +1,10 @@
 import fitz
 import regex as re
 from nltk import sent_tokenize
+import tensorflow as tf
+import tensorflow_text
+import requests
+
 
 def check_contain(r_word, points):
     r = fitz.Quad(points).rect
@@ -66,3 +70,73 @@ def extract_context(excerpt, document, size=400):
     context = ' '.join(sent_tokenize(context)[1:-1])
     
     return context
+
+
+def download_model():
+    saved_model = requests.get('https://storage.googleapis.com/decontext_dataset/t5_base/1611267950/saved_model.pb')
+    open('model/saved_model.pb', 'wb').write(saved_model.content)
+
+    variables0 = requests.get('https://storage.googleapis.com/decontext_dataset/t5_base/1611267950/variables/variables.data-00000-of-00002')
+    open('model/variables/variables.data-00000-of-00002', 'wb').write(variables0.content)
+
+    variables1 = requests.get('https://storage.googleapis.com/decontext_dataset/t5_base/1611267950/variables/variables.data-00001-of-00002')
+    open('model/variables/variables.data-00001-of-00002', 'wb').write(variables1.content)
+
+    variables_index = requests.get('https://storage.googleapis.com/decontext_dataset/t5_base/1611267950/variables/variables.index')
+    open('model/variables/variables.index', 'wb').write(variables_index.content)
+
+
+def load_predict_fn():
+    imported = tf.saved_model.load('./model', ['serve'])
+    return lambda x: imported.signatures['serving_default'](tf.constant(x))['outputs'].numpy()
+
+
+def decontextualize(input):
+  return predict_fn([input])[0].decode('utf-8')
+
+
+# download_model()
+predict_fn = load_predict_fn()
+
+paragraph = [
+  "Gagarin was a keen sportsman and played ice hockey as a goalkeeper.",
+  "He was also a basketball fan and coached the Saratov Industrial Technical School team, as well as being a referee.",
+  "In 1957, while a cadet in flight school, Gagarin met Valentina Goryacheva at the May Day celebrations at the Red Square in Moscow.",
+  "She was a medical technician who had graduated from Orenburg Medical School.",
+  "They were married on 7 November of the same year, the same day Gagarin graduated from his flight school, and they had two daughters.",
+  "Yelena Yurievna Gagarina, born 1959, is an art historian who has worked as the director-general of the Moscow Kremlin Museums since 2001; and Galina Yurievna Gagarina, born 1961, is a professor of economics and the department chair at Plekhanov Russian University of Economics in Moscow."
+]
+
+page_title = 'Yuri Gagarin'
+section_title = 'Personal Life'  # can be empty
+target_sentence_idx = 4  # zero-based index
+
+
+if target_sentence_idx >= len(paragraph) or target_sentence_idx < 0:
+  raise ValueError(
+      f'Target sentence index must be in range [0, {len(paragraph) - 1}].')
+
+
+def create_input(paragraph,
+                 target_sentence_idx,
+                 page_title='',
+                 section_title=''):
+  """Creates a single Decontextualization example input for T5.
+
+  Args:
+    paragraph: List of strings. Each string is a single sentence.
+    target_sentence_idx: Integer index into `paragraph` indicating which
+      sentence should be decontextualized.
+    page_title: Optional title string. Usually Wikipedia page title.
+    section_title: Optional title of section within page.
+  """
+  prefix = ' '.join(paragraph[:target_sentence_idx])
+  target = paragraph[target_sentence_idx]
+  suffix = ' '.join(paragraph[target_sentence_idx + 1:])
+  return ' [SEP] '.join((page_title, section_title, prefix, target, suffix))
+
+d = decontextualize(
+        create_input(paragraph, target_sentence_idx, page_title,
+                     section_title))
+print(f'Original sentence:         {paragraph[target_sentence_idx]}\n'
+      f'Decontextualized sentence: {d}')
