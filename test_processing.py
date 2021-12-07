f150f95bed5d203ebc702c61047a7ce3fc56b7bb
Paul Bricman <paulbricman@protonmail.com>
2021-12-07 08:13:46 +0100

    feat: use model on excerpts

--- test_processing.py
+++ test_processing.py
@@ -1,12 +1,21 @@
-from processing import pdf_to_text, pdf_to_excerpts, extract_context
+from processing import *
 
 
 def test_context_extraction():
+    download_model()
+    predict_fn = load_predict_fn()
+
+    print('[*] Model loaded')
+
     filename = 'data/annotated1.pdf'
     document = pdf_to_text(filename)
     excerpts = pdf_to_excerpts(filename)
-    
     contexts = [extract_context(excerpt, document) for excerpt in excerpts]
-    print(*[e for e in zip(excerpts, contexts)], sep='\n\n')
+    
+    print('[*] Excerpts and contexts loaded')
+
+    for e_idx, e in enumerate(zip(excerpts, contexts)):
+        input = create_input(e[0], e[1])
+        output = decontextualize_excerpt(e[0], e[1], predict_fn)
 
 test_context_extraction()
